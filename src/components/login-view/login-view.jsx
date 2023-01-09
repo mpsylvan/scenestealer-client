a25d7b48821362dc8d1c182502ced7234246e98c
Michael <micperdo@gmail.com>
2023-01-08 20:37:22 -0500

    includes JWT in local storage for persisted auth

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -6,69 +6,71 @@ import {useState} from "react";
 export const LoginView = ({onLoggedIn}) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
     const handleSubmit = (event) => {
         // prevents the default behavior of the form which is to reload the entire page
         event.preventDefault();
-
-        // sample data to test another login flow via open library
-        // const libData = {
-        //     access: username, //167OLdP5BUfLZGxP
-        //     secret: password //K39eKYhPMV9DDWhJ
-        // };
 
         const data = {
             Username: username,
             Password: password
         }
 
-        // testing request for troubleshooting
-        // fetch("https://openlibrary.org/account/login.json", {
-        // method: "POST",
-        // body: JSON.stringify(libData)
-        // }).then((response)=>{
-        //     if(response.ok){
-        //         onLoggedIn(username)
-        //     }else{
-        //         alert('library login failed')
-        //     }
-        // });
-
+        
+        
         fetch("https://scenestealer.herokuapp.com/login", {
             method: "POST",
             body: JSON.stringify(data)
-        }).then((response)=>{
-            if(response.ok){
-                onLoggedIn(username);
+        }).then((response)=>response.json())
+          .then((data)=>{
+            console.log(data)
+            if(data.user){
+                localStorage.setItem("user", JSON.stringify(data.user))
+                localStorage.setItem("token", JSON.stringify(data.token))
+                onLoggedIn(data.user, data.token);
             }else{
-                alert('movie login failed')
-                // onLoggedIn(username);
+                alert('no such user');
             }
-        });
+          })
+            .catch((e)=>{
+                console.log(e);
+                alert('something went wrong');
+            })
+       
+        
   };
 
+
+
+
   return (
-    <form onSubmit={handleSubmit}>
-        <label>
-            Username:
-        </label>
-        <input 
-            type="text"
-            value = {username}
-            onChange = {(e)=>setUsername(e.target.value)}
-            required
-            />
-        <label>
-            Password:
-        </label>
-        <input 
-            type="password"
-            value = {password}
-            onChange = {(e)=>setPassword(e.target.value)}
-            required
-            />
-        <button type="submit">
-            Submit
-        </button>
-    </form>
+    <>
+        <h3>Login here: </h3>
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+            </label>
+            <input 
+                type="text"
+                value = {username}
+                onChange = {(e)=>{setUsername(e.target.value)}}
+                required
+                />
+            <label>
+                Password:
+            </label>
+            <input 
+                type="password"
+                value = {password}
+                onChange = {(e)=>{setPassword(e.target.value)}}
+                required
+                />
+            <button type="submit">
+                Submit
+            </button>
+        </form>
+        
+    
+    </>
   );
 };
