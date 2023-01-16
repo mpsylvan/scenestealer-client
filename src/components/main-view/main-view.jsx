697a0ab1c457ba1218a4aaf619453f2224970096
Michael <micperdo@gmail.com>
2023-01-16 15:42:53 -0500

    corrects main view return statement

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -141,17 +141,3 @@ export const MainView = ()=>{
     };
     
     
-    // finally, MainView renders each movie object as a MovieCard
-    return (
-        <div>
-            {movies.map((movie) => 
-            (<MovieCard key={movie.key} 
-                movie={movie} 
-                onMovieClick = {(movie)=> setSelectedMovie(movie)}/>
-            ))}
-            <button onClick={()=>{setUser(null); setToken(null)}}> Logout </button>
-        </div>
-    );
-     
-
-};
