5344a4cb189810f3629f901e07c3a8a6b950de71
Michael <micperdo@gmail.com>
2023-02-02 13:53:02 -0500

    updates file name to avoid any case-sensitivies in build

--- src/redux/reducers/users/user.js
+++ src/redux/reducers/users/user.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { loadState } from "../../../localstorage";
+import { loadState } from "../../../localStorage";
 
 const persistedState = loadState();
 
