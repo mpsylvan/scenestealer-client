33f1f50dd5a06c5ece2216acad52dc01b04dfc1b
Michael <micperdo@gmail.com>
2023-02-02 14:51:51 -0500

    updates logic of profile view to display unsubmitted date

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -3,14 +3,20 @@ import {Card, Button} from 'react-bootstrap';
 
 
 export const UserInfo = ({email, username, birthdate,  handleDeregister}) =>{
-
-    const bd = birthdate.split("T")[0];
-
+    let bd;
+    if(birthdate){
+        bd = birthdate.split("T")[0];
+        console.log(bd)
+    }else{
+        bd = "Unsubmitted"
+    }
+   
     return(
             <>
                 <h4> Username: {username}</h4>   
                 <h4> Email: {email}</h4>
                 <h4> Birthday: {bd}</h4>
+                
                 <Button variant = "danger" type ="submit" onClick={()=>handleDeregister(username)}> Deregister </Button> 
             </>
         )
